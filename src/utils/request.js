7b3035f91a8ae237ff01d348306340f7e2cc6a76
Simuliya <1099409644@qq.com>
2025-04-11 10:49:03 +0800

    更新uniapp的request适配

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,87 +1,79 @@
-import axios from "axios";
-import {getToken, removeToken} from "@/utils/auth"
-import {toast} from '@/utils/common'
-
+import { getToken, removeToken } from '@/utils/auth.js'
+import { toast } from '@/utils/common'
 
 export const baseUrl = "http://localhost:8160"
-// 创建请求实例
-const instance = axios.create({
-    // 默认配置
-    baseURL: baseUrl,
-    timeout: 10000,
 
-})
+export default function ajax(config) {
+    const {
+        url = '',
+        method = 'get',
+        data = {},
+        header = {},
+    } = config
 
-// 添加请求拦截器
-instance.interceptors.request.use(config => {
-        // 在发送请求前做些什么
-        uni.showLoading({
-            title: '加载中...'
-        })
-        const isToken = config.headers['isToken'] === false
-        if (getToken() && !isToken) {
-            config.headers['Authorization'] = 'Bearer ' + getToken()
-        }
-        return config
-    },
-    error => {
-        // 对请求错误做些什么
-        uni.showToast({
-            icon: 'error',
-            title: '后端接口请求异常'
-        })
-        return Promise.reject(error)
+    // 开启加载
+    uni.showLoading({
+        title: '加载中...',
+    })
+
+    // 默认 headers，加上 Authorization
+    const isToken = header['isToken'] === false
+    const token = getToken()
+    const finalHeaders = {
+        'Content-Type': 'application/json',
+        ...header,
     }
-)
 
-// 添加响应拦截器
-instance.interceptors.response.use(
-    //响应成功后
-    response => {
-        uni.hideLoading()
+    if (token && !isToken) {
+        finalHeaders['Authorization'] = 'Bearer ' + token
+    }
 
-        // 未设置状态码则默认成功状态
-        const code = response.data.code || 200;
-        // 获取错误信息
-        const msg = response.data.msg
-        if (code == 401) {
-            removeToken();
-            uni.showModal({
-                title: '提示',
-                content: '登录状态已过期，您可以继续留在该页面，或者重新登录?',
-                confirmText: '登录',
-                success: function (res) {
-                    if (res.confirm) {
-                        uni.navigateTo({
-                            url: '/pages/me/me'
-                        })
-                        return Promise.reject("")
-                    } else if (res.cancel) {
-                        // console.log('用户点击取消');
-                    }
+    return new Promise((resolve, reject) => {
+        uni.request({
+            url: baseUrl + url,
+            method: method.toUpperCase(),
+            data,
+            header: finalHeaders,
+            success: (res) => {
+                const code = res.data.code || 200
+                const msg = res.data.msg || '请求异常'
+
+                if (code === 401) {
+                    removeToken()
+                    uni.showModal({
+                        title: '提示',
+                        content: '登录状态已过期，您可以继续留在该页面，或者重新登录？',
+                        confirmText: '登录',
+                        success: function (modalRes) {
+                            if (modalRes.confirm) {
+                                uni.navigateTo({
+                                    url: '/pages/me/me',
+                                })
+                            }
+                        },
+                    })
+                    toast('无效的会话，或者会话已过期，请重新登录。')
+                    reject('无效的会话，或者会话已过期，请重新登录。')
+                } else if (code === 500) {
+                    toast(msg)
+                    reject(msg)
+                } else if (code !== 200) {
+                    toast(msg)
+                    reject(msg)
+                } else {
+                    resolve(res)
                 }
-            });
-            toast('无效的会话，或者会话已过期，请重新登录。')
-            return Promise.reject("无效的会话，或者会话已过期，请重新登录。")
-        } else if (code == 500) {
-            toast(msg)
-            return Promise.reject(msg)
-        } else if (code !== 200) {
-            toast(msg)
-            return Promise.reject(msg)
-        }
-        return response
-    },
-    error => {
-        uni.hideLoading()
-
-        // 对响应错误做些什么
-        uni.showToast({
-            icon: 'error',
-            title: '后端接口响应异常'
+            },
+            fail: (err) => {
+                uni.showToast({
+                    icon: 'error',
+                    title: '后端接口请求异常',
+                })
+                reject(err)
+            },
+            complete: () => {
+                uni.hideLoading()
+            },
         })
-        return Promise.reject(error)
-    }
-)
-
-export default instance
+    })
+}
